5f072a5fc69f16f9a3c408298e761f9deaa9556c
Alex <blackbird0x71@gmail.com>
2026-01-27 19:28:12 +0300

    Update patch.py

--- patch.py
+++ patch.py
@@ -1,1 +1,14 @@
-Hello World
+def wrapper(*args, **kwargs):
+  if args and isinstance(args[0], str) and self._enrich:
+      msg = self._enrich(args[0])
+      args = (msg, *args[1:])
+  
+  if self._enrich:
+      # только для prod-использования
+      logger = self._logger
+      if hasattr(logger, "opt"):
+          logger = logger.opt(depth=2)
+      return getattr(logger, name)(*args, **kwargs)
+  else:
+      # для тестов, чтобы mock_logger видел вызовы напрямую
+      return attr(*args, **kwargs)
